44965b52ad7ced872d1892c61cff46e40ee79e86
athenas2021 <lucas.kano@outlook.com>
2021-12-04 09:35:28 -0300

    templates

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, TesteView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
 ]
