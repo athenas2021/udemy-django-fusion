030491129f57539e7e374dd56f31fa6321b052de
athenas2021 <lucas.kano@outlook.com>
2021-12-05 08:54:34 -0300

    Views -> Services, Team

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from .models import Service, Team
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['services'] = Service.objects.filter(status=True).order_by('?')
+        context['team'] = Team.objects.filter(status=True).order_by('?')
+        return context
