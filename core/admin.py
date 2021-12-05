3f902077be3eee15dcfc67e84fe90e7caef777a0
athenas2021 <lucas.kano@outlook.com>
2021-12-05 08:32:37 -0300

    Admin panel...

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Position, Service, Team
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('position', 'status', 'updated')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'icon', 'status', 'updated')
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'status', 'updated')
